642cefb8a5043628d024fcfd77e6480d9a7d2314
Vladimir Kuleshov <gottgunst@gmail.com>
2023-05-11 18:18:49 +0300

    Блок цифр для десктопа

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -7,7 +7,34 @@ export function Service() {
     document.querySelector('header')?.classList.remove('header_inverted');
   },[]);
 
-  return (
+  return (<>
+    <br/><br/>
+    <br/><br/>
+    <br/><br/>
+    <br/><br/>
+
+    <section className="section section_type_numbers">
+      <ul className="grid grid_type_numbers">
+        <li>
+          <h2 className="section__title numbers__digit" data-above='more'>250</h2>
+          <p className="section__text numbers__caption">сотрудников компании</p>
+        </li>
+        <li>
+          <h2 className="section__title numbers__digit" data-above='more'>10 000</h2>
+          <p className="section__text numbers__caption">реализованных проектов</p>
+        </li>
+        <li>
+          <h2 className="section__title numbers__digit">22</h2>
+          <p className="section__text numbers__caption">года работы</p>
+        </li>
+        <li>
+          <h2 className="section__title numbers__digit" data-above='more'>30</h2>
+          <p className="section__text numbers__caption">брендов</p>
+        </li>
+      </ul>
+    </section>
+
+
     <section className="section">
       <h2 className="section__title">Сервис</h2>
       <ul className="grid grid_type_service">
@@ -43,5 +70,5 @@ export function Service() {
         </article></li>
       </ul>
     </section>
-  );
+    </>);
 }
